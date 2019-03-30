8d825d887e0b2bb0b89ad36fda470a0fa0dc8036
qdrka_hw <bermetursunalieva@gmail.com>
2019-03-30 19:07:02 +0600

    qdrka_hw

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("hello,world!");
+	String a = "hello";
+	String b = "world";
+	String c = a+b;
+	System.out.println(c);
+    }
+}
